3d44faba6e2ea46ad6fc0ce3c0b2a5e27e5df1d1
Jumanalieva <Sadirbekova12@gmail.com>
2023-06-09 20:30:19 -0400

    First commitment

---
+++ Ex_1.2_ d.types/1.2_WorldPopulation.py
@@ -0,0 +1,9 @@
+
+total_population = (6789088686, 6872767093, 6956823603, 7041194301, 7125828059, 7210581976, 7295290765, 737997139, 7464022049, 7547858925, 7631091040, 7713468100, 7794798739)
+print(total_population)
+
+sliced_total_population = print(total_population[0:13:3])
+
+sliced_total_population = print(max(total_population))
+
+
---
+++ Ex_1.2_ d.types/1.3_Ford_v_lineup.py
@@ -0,0 +1,12 @@
+
+Ford_vehicles_2020 =['Fiesta', 'Focus', 'Mondeo', 'Galaxy','Edge', 'Ecosport', 'Puma', 'Ranger']
+print(Ford_vehicles_2020)
+
+Ford_vehicles_2020.append('Mustang')
+print(Ford_vehicles_2020)
+
+Ford_vehicles_2020.sort()
+print(Ford_vehicles_2020)
+
+Ford_vehicles_2020.sort(reverse = True)
+print(Ford_vehicles_2020)
